2a0b0a0b9bc7fcbe5ff186d2f087f16f5744211a
Steve <braunboyz@gmail.com>
2019-02-26 18:07:46 -0500

    Added radius removal outlier functionality

--- cal/src/outlier3.cpp
+++ cal/src/outlier3.cpp
@@ -2,11 +2,13 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/filters/radius_outlier_removal.h>
 
 int K;
 float T;
 int scan_val;
 std::string filename;
+bool rad_rem;
 
 int parseArgument(char* arg)
 {
@@ -27,9 +29,15 @@ int parseArgument(char* arg)
       T = foption;
       return 0;
     }
-    if(1==sscanf(arg,"-f%s", buf))
+    if(1==sscanf(arg,"-%s", buf))
     {
-      return 1;
+      if(strcmp(buf, "f") == 0)
+        return 1;
+      else if(strcmp(buf, "r") == 0)
+      {
+        rad_rem = true;
+        return 0;
+      }
     }
   }
   else if(scan_val == 1)
@@ -47,9 +55,12 @@ int parseArgument(char* arg)
 
 int main (int argc, char** argv)
 {
+  // defaults
   filename = "phone.pcd";
+  rad_rem = false;
   K = 100;
   T = 0.01;
+
   scan_val = 0;
   int i = 1;
   while(i < argc)
@@ -60,36 +71,47 @@ int main (int argc, char** argv)
 
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGB>);
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_outliers (new pcl::PointCloud<pcl::PointXYZRGB>);
-  // Fill in the cloud data
-  pcl::PCDReader reader;
-  // Replace the path below with the path where you saved your file
-  reader.read<pcl::PointXYZRGB> ("../dat/pcl/" + filename, *cloud);
+  //pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_outliers (new pcl::PointCloud<pcl::PointXYZRGB>);
+  pcl::io::loadPCDFile ("../dat/pcl/" + filename, *cloud);
 
-  std::cerr << "Cloud before filtering: " << std::endl;
-  std::cerr << *cloud << std::endl;
+  std::cout << "Cloud before filtering: " << std::endl;
+  std::cout << *cloud << std::endl;
 
-  // Create the filtering object
-  pcl::StatisticalOutlierRemoval<pcl::PointXYZRGB> sor;
-  sor.setInputCloud (cloud);
-  sor.setMeanK (K);
-  sor.setStddevMulThresh (T);
-  sor.filter (*cloud_filtered);
+  if(rad_rem)
+  {
+    pcl::RadiusOutlierRemoval<pcl::PointXYZRGB> outrem;
+    // build the filter
+    outrem.setInputCloud(cloud);
+    outrem.setRadiusSearch(T);
+    outrem.setMinNeighborsInRadius (K);
+    // apply filter
+    outrem.filter (*cloud_filtered);
+  }
+  else
+  {
+    // Create the filtering object
+    pcl::StatisticalOutlierRemoval<pcl::PointXYZRGB> sor;
+    sor.setInputCloud (cloud);
+    sor.setMeanK (K);
+    sor.setStddevMulThresh (T);
+    sor.filter (*cloud_filtered);
+  }
 
-  std::cerr << "Cloud after filtering: " << std::endl;
-  std::cerr << *cloud_filtered << std::endl;
+  std::cout << "Cloud after filtering: " << std::endl;
+  std::cout << *cloud_filtered << std::endl;
 
   pcl::PCDWriter writer;
-  writer.write<pcl::PointXYZRGB> ("../dat/filt/cloud_inliers.pcd", *cloud_filtered, false);
+  writer.write<pcl::PointXYZRGB> ("../dat/filt/" + filename, *cloud_filtered, false);
 
-
+/*
   sor.setNegative (true);
   sor.filter (*cloud_outliers);
   
   std::cerr << "Cloud outliers: " << std::endl;
   std::cerr << *cloud_outliers << std::endl;
-  
-  writer.write<pcl::PointXYZRGB> ("../dat/filt/cloud_outliers.pcd", *cloud_outliers, false);
+  */
+  //writer.write<pcl::PointXYZRGB> ("../dat/filt/cloud_outliers.pcd", *cloud_outliers, false);
 
-exit(0);
+  return 0;
+  //exit(0);
 }
