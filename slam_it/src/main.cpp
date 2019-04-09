9e6e34b7818e0f740048334b68d0f82bacae6a7f
Steve <braunboyz@gmail.com>
2019-04-09 14:31:30 -0400

    Added new sub-repo for final slam algorithm

---
+++ slam_it/src/main.cpp
@@ -0,0 +1,236 @@
+/**
+* This file is part of DSO.
+* 
+* Copyright 2016 Technical University of Munich and Intel.
+* Developed by Jakob Engel <engelj at in dot tum dot de>,
+* for more information see <http://vision.in.tum.de/dso>.
+* If you use this code, please cite the respective publications as
+* listed on the above website.
+*
+* DSO is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* DSO is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with DSO. If not, see <http://www.gnu.org/licenses/>.
+*
+* *MODIFIED*
+*/
+
+
+#include <thread>
+#include <locale.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "IOWrapper/Output3DWrapper.h"
+#include "IOWrapper/ImageDisplay.h"
+
+
+#include <boost/thread.hpp>
+#include "util/settings.h"
+#include "util/globalFuncs.h"
+#include "util/DatasetReader.h"
+#include "util/globalCalib.h"
+
+#include "util/NumType.h"
+#include "FullSystem/FullSystem.h"
+#include "OptimizationBackend/MatrixAccumulators.h"
+#include "FullSystem/PixelSelector2.h"
+
+#include "IOWrapper/OutputWrapper/CustomWrapper.h"
+
+
+std::string source = "";
+std::string calib = "";
+double rescale = 1;
+float playbackSpeed=0;	// 0 for linearize (play as fast as possible, while sequentializing tracking & mapping). otherwise, factor on timestamps.
+bool useStdOutput=false;
+bool delete_img = false;
+
+int mode=0;
+
+
+using namespace dso;
+
+
+void my_exit_handler(int s)
+{
+	printf("Caught signal %d\n",s);
+	exit(1);
+}
+
+void exitThread()
+{
+	struct sigaction sigIntHandler;
+	sigIntHandler.sa_handler = my_exit_handler;
+	sigemptyset(&sigIntHandler.sa_mask);
+	sigIntHandler.sa_flags = 0;
+	sigaction(SIGINT, &sigIntHandler, NULL);
+
+	while(true) pause();
+}
+
+
+void settingsDefault(int preset)
+{
+	printf("\n=============== PRESET Settings: ===============\n");
+	if(preset == 0 || preset == 1)
+	{
+		printf("DEFAULT settings:\n"
+				"- %s real-time enforcing\n"
+				"- 2000 active points\n"
+				"- 5-7 active frames\n"
+				"- 1-6 LM iteration each KF\n"
+				"- original image resolution\n", preset==0 ? "no " : "1x");
+
+		playbackSpeed = (preset==0 ? 0 : 1);
+		setting_desiredImmatureDensity = 1500;
+		setting_desiredPointDensity = 2000;
+		setting_minFrames = 5;
+		setting_maxFrames = 7;
+		setting_maxOptIterations=6;
+		setting_minOptIterations=1;
+
+		setting_logStuff = false;
+	}
+
+	if(preset == 2 || preset == 3)
+	{
+		printf("FAST settings:\n"
+				"- %s real-time enforcing\n"
+				"- 800 active points\n"
+				"- 4-6 active frames\n"
+				"- 1-4 LM iteration each KF\n"
+				"- 424 x 320 image resolution\n", preset==0 ? "no " : "5x");
+
+		playbackSpeed = (preset==2 ? 0 : 5);
+		setting_desiredImmatureDensity = 600;
+		setting_desiredPointDensity = 800;
+		setting_minFrames = 4;
+		setting_maxFrames = 6;
+		setting_maxOptIterations=4;
+		setting_minOptIterations=1;
+
+		benchmarkSetting_width = 424;
+		benchmarkSetting_height = 320;
+
+		setting_logStuff = false;
+	}
+
+	printf("==============================================\n");
+}
+
+
+
+
+
+
+void parseArgument(char* arg)
+{
+	int option;
+	float foption;
+	char buf[1000];
+
+
+    if(1==sscanf(arg,"quiet=%d",&option))
+    {
+        if(option==1)
+        {
+            setting_debugout_runquiet = true;
+            printf("QUIET MODE, I'll shut up!\n");
+        }
+        return;
+    }
+
+	if(1==sscanf(arg,"preset=%d",&option))
+	{
+		settingsDefault(option);
+		return;
+	}
+
+
+	if(1==sscanf(arg,"nolog=%d",&option))
+	{
+		if(option==1)
+		{
+			setting_logStuff = false;
+			printf("DISABLE LOGGING!\n");
+		}
+		return;
+	}
+	if(1==sscanf(arg,"nomt=%d",&option))
+	{
+		if(option==1)
+		{
+			multiThreading = false;
+			printf("NO MultiThreading!\n");
+		}
+		return;
+	}
+
+	if(1==sscanf(arg,"files=%s",buf))
+	{
+		source = buf;
+		printf("loading data from %s!\n", source.c_str());
+		return;
+	}
+
+	if(1==sscanf(arg,"calib=%s",buf))
+	{
+		calib = buf;
+		printf("loading calibration from %s!\n", calib.c_str());
+		return;
+	}
+
+
+	if(1==sscanf(arg,"speed=%f",&foption))
+	{
+		playbackSpeed = foption;
+		printf("PLAYBACK SPEED %f!\n", playbackSpeed);
+		return;
+	}
+/*
+	// New stuffs
+	if(1==sscanf(arg,"pose=%s",buf))
+	{
+		pose_source = buf;
+		printf("loading pose info from %s!\n", pose_source.c_str());
+		return;
+	}*/
+
+	printf("could not parse argument \"%s\"!!!!\n", arg);
+}
+
+
+
+int main( int argc, char** argv )
+{
+    // default values
+    setting_debugout_runquiet = true;
+    settingsDefault(0);
+    mode = 1;
+    setting_photometricCalibration = 0;
+    setting_affineOptModeA = 0; //-1: fix. >=0: optimize (with prior, if > 0).
+    setting_affineOptModeB = 0; //-1: fix. >=0: optimize (with prior, if > 0).
+    useStdOutput = true;
+    delete_img = false;
+    // parse arguments
+    for(int i=1; i<argc;i++)
+        parseArgument(argv[i]);
+
+    // hook crtl+C.
+    boost::thread exThread = boost::thread(exitThread);
+
+
+    ImageFolderReader* reader = new ImageFolderReader(source, calib, "", "");
+    reader->setGlobalCalibration();
+}
