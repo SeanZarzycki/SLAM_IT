78c6641c165f3dd0cc96fb5440826b53e9244c8d
Steve <braunboyz@gmail.com>
2019-04-15 21:34:52 -0400

    Added external file for faster testing

---
+++ cal/src/Fuse_help.cpp
@@ -0,0 +1,24 @@
+
+/* Filter */
+// filter type: 1 for radius, 2 for stat
+int filt_type = 2;
+// NUmber of neighbors required
+int filt_N = 100;
+// search radius
+float filt_R = 0.05f;
+
+/* SIFT Detection */
+// min scale
+float sift_ms = 0.04f;
+// number of octaves
+int sift_no = 6;
+// scales per octave
+int sift_ns = 10;
+// min contrast
+int sift_mc = 0.0001f;
+
+/* Normals Calculation */
+float norm_r = 0.02f;
+
+/* Feature Calculation */
+float feat_r = 0.06f;
