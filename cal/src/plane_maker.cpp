cea0459438f35455892db7db668eb4c2a4c4d5f3
Tim <timthekoolaidman@gmail.com>
2019-04-28 16:31:45 -0400

    creates pyramids out of points

---
+++ cal/src/plane_maker.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <fstream>
+#include <string>
+
+using namespace std;
+int main (int argc, char** argv)
+{
+    ofstream out_file;
+    // defaults
+    
+    string filename = argv[1];
+    double delta = .00000001;
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::io::loadPCDFile ("../dat/pcl/" + filename, *cloud);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud2 (new pcl::PointCloud<pcl::PointXYZRGB>);
+    cloud2->resize(4*cloud->size());
+    //out_file.open("../dat/pcl/" + head + ".obj");
+    for(int i=0; i < cloud->size(); i++)
+    {
+        // top
+        int j = 0;
+        cloud2->points[4*i+j].x = cloud->points[i].x;
+        cloud2->points[4*i+j].y = cloud->points[i].y;
+        cloud2->points[4*i+j].z = cloud->points[i].z + delta;
+
+        // side 1
+        j = 1;
+        cloud2->points[4*i+j].x = cloud->points[i].x;
+        cloud2->points[4*i+j].y = cloud->points[i].y + delta;
+        cloud2->points[4*i+j].z = cloud->points[i].z;
+
+
+        // side 2
+        j = 2;
+        cloud2->points[4*i+j].x = cloud->points[i].x + delta;
+        cloud2->points[4*i+j].y = cloud->points[i].y;
+        cloud2->points[4*i+j].z = cloud->points[i].z;
+
+        // side 3
+        j = 3;
+        cloud2->points[4*i+j].x = cloud->points[i].x;
+        cloud2->points[4*i+j].y = cloud->points[i].y;
+        cloud2->points[4*i+j].z = cloud->points[i].z;
+
+        for(j = 0;j < 4;j++)
+        {
+            cloud2->points[4*i+j].r = cloud->points[i].r;
+            cloud2->points[4*i+j].g = cloud->points[i].g;
+            cloud2->points[4*i+j].b = cloud->points[i].b;
+        }
+    }
+
+     pcl::PCDWriter writer;
+     writer.write<pcl::PointXYZRGB> ("../dat/pcl/exp_" + filename, *cloud2, false);
+
+    return 0;
+}
