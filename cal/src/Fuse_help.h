bc9e6a0d10e74f342703e22246fa7ac26e85c1b2
Steve <braunboyz@gmail.com>
2019-05-13 20:26:23 -0400

    Move external variables to a .h file

---
+++ cal/src/Fuse_help.h
@@ -0,0 +1,26 @@
+
+
+// constant values for testing
+extern int filt_type;
+extern int filt_N;
+extern float filt_R;
+extern int filt_K;
+extern float filt_T;
+extern float sift_ms;
+extern int sift_no;
+extern int sift_ns;
+extern int sift_mc;
+extern float sift_prct;
+extern float norm_r;
+extern float feat_r;
+extern float filt_dres;
+extern float extr_nsz;
+extern float corr_eps;
+extern int corr_n;
+extern float extr_mksz;
+extern int keys_mode;
+extern float harr_rad;
+extern bool harr_maxs;
+extern bool harr_ref;
+extern float harr_tau;
+extern int feat_mode;
