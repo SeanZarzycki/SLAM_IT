efd05056eebf1228e27285a75edf761b48101ed7
Steve <braunboyz@gmail.com>
2019-05-20 22:13:50 -0400

    now can view 2 at once with transform

--- cal/src/main3.cpp
+++ cal/src/main3.cpp
@@ -9,7 +9,10 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/console/parse.h>
+#include <pcl/common/transforms.h>
 
+
+bool color_div;
 
 // --------------
 // -----Help-----
@@ -41,26 +44,15 @@ pcl::visualization::PCLVisualizer::Ptr rgbVis (pcl::PointCloud<pcl::PointXYZRGB>
   pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
   viewer->setBackgroundColor (0, 0, 0);
   pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(cloud);
-  viewer->addPointCloud<pcl::PointXYZRGB> (cloud, rgb, "sample cloud");
-  viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "sample cloud");
-  //viewer->addCoordinateSystem (1.0);
+  viewer->addPointCloud<pcl::PointXYZRGB> (cloud, rgb, "Cloud 1");
+  viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "Cloud 1");
+  if(color_div)
+      viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 1, 0, 0, "Cloud 1");
+  viewer->addCoordinateSystem (0.2);
   viewer->initCameraParameters ();
+  viewer->setCameraPosition(0, 0, -2, 0, 0, 1, 0, 1, 0);
   return (viewer);
 }
-
-
-
-void color_correct(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud)
-{
-  for(size_t i = 0;i < cloud->points.size();i++)
-  {
-    float temp = (cloud->points[i].r + cloud->points[i].g + cloud->points[i].b) / 3.0;
-    cloud->points[i].r = temp;
-    cloud->points[i].g = temp;
-    cloud->points[i].b = temp;
-  }
-}
-
 
 
 // --------------
@@ -68,14 +60,25 @@ void color_correct(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud)
 // --------------
 int main (int argc, char** argv)
 {
+  bool two_en = false;
+  color_div = false;
   // get filename
-  std::string filename;
+  std::string filename, filename2;
   int temp = pcl::console::find_argument (argc, argv, "-f");
   if(temp >= 0 && temp < argc)
     filename = argv[temp+1];
   else
     filename = "cloud_inliers.pcd";
 
+  temp = pcl::console::find_argument (argc, argv, "-t");
+  if(temp >= 0 && temp < argc)
+  {
+    two_en = true;
+    filename2 = argv[temp+1];
+  }
+  temp = pcl::console::find_argument (argc, argv, "-c");
+  if(temp > 0)
+    color_div = !color_div;
   // --------------------------------------
   // -----Parse Command Line Arguments-----
   // --------------------------------------
@@ -84,71 +87,46 @@ int main (int argc, char** argv)
     printUsage (argv[0]);
     return 0;
   }
-  bool simple(false), rgb(false), custom_c(false), normals(false),
-    shapes(false), viewports(false), interaction_customization(false);
   
 
-    rgb = true;
     std::cout << "RGB colour visualisation example\n";
 
 
   // ------------------------------------
   // -----Create example point cloud-----
   // ------------------------------------
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr point_cloud_ptr (new pcl::PointCloud<pcl::PointXYZRGB>);
-  pcl::io::loadPCDFile ("../dat/filt/" + filename, *point_cloud_ptr);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud1 (new pcl::PointCloud<pcl::PointXYZRGB>);
+  pcl::io::loadPCDFile ("../dat/fuse_dataset/" + filename, *cloud1);
 
-  //color_correct(point_cloud_ptr);
-
- /* std::cout << "Generating example point clouds.\n\n";
-  // We're going to make an ellipse extruded along the z-axis. The colour for
-  // the XYZRGB cloud will gradually go from red to green to blue.
-  uint8_t r(255), g(15), b(15);
-  for (float z(-1.0); z <= 1.0; z += 0.05)
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud2 (new pcl::PointCloud<pcl::PointXYZRGB>);
+  if(two_en)
   {
-    for (float angle(0.0); angle <= 360.0; angle += 5.0)
-    {
-      pcl::PointXYZ basic_point;
-      basic_point.x = 0.5 * cosf (pcl::deg2rad(angle));
-      basic_point.y = sinf (pcl::deg2rad(angle));
-      basic_point.z = z;
-      basic_cloud_ptr->points.push_back(basic_point);
+    Eigen::Matrix4f s2d;
+    s2d <<  -0.00508278,     1.27822 ,  0.0106919 ,  0.0634904,
+            -1.23778, -0.00759102 ,   0.319097 ,  -0.314879,
+            0.319147, -0.00908433 ,    1.23776 , -0.0596386,
+            0,           0,           0     ,      1;
+    /*s2d << 0, 1, 0, 0,
+          -1, 0, 0, 0,
+           0, 0, 1, 0,
+           0, 0, 0, 1;*/
 
-      pcl::PointXYZRGB point;
-      point.x = basic_point.x;
-      point.y = basic_point.y;
-      point.z = basic_point.z;
-      uint32_t rgb = (static_cast<uint32_t>(r) << 16 |
-              static_cast<uint32_t>(g) << 8 | static_cast<uint32_t>(b));
-      point.rgb = *reinterpret_cast<float*>(&rgb);
-      point_cloud_ptr->points.push_back (point);
-    }
-    if (z < 0.0)
-    {
-      r -= 12;
-      g += 12;
-    }
-    else
-    {
-      g -= 12;
-      b += 12;
-    }
-  } */
-  point_cloud_ptr->width = (int) point_cloud_ptr->points.size ();
-  point_cloud_ptr->height = 1;
 
-  // ----------------------------------------------------------------
-  // -----Calculate surface normals with a search radius of 0.05-----
-  // ----------------------------------------------------------------
-  
-  // ---------------------------------------------------------------
-  // -----Calculate surface normals with a search radius of 0.1-----
-  // ---------------------------------------------------------------
-  
+    pcl::io::loadPCDFile ("../dat/fuse_dataset/" + filename2, *cloud2);
+    pcl::transformPointCloud(*cloud2, *cloud2, s2d);
+  }
+
+
   pcl::visualization::PCLVisualizer::Ptr viewer;
   
-    viewer = rgbVis(point_cloud_ptr);
-  
+  viewer = rgbVis(cloud1);
+  if(two_en)
+  {
+    viewer->addPointCloud<pcl::PointXYZRGB> (cloud2, "Cloud 2");
+    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "Cloud 2");
+    if(color_div)
+      viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 0, 0, 1, "Cloud 2");
+  }
 
   //--------------------
   // -----Main loop-----
